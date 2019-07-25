41ae58c97aa315ea2a3e3b9bf1af466c12b60b78
ohsangheon <ozzang1991@gmail.com>
2019-07-25 14:33:02 +0900

    bitcamp-java-application v28 완료

---
+++ bitcamp-java-basic/src/main/java/ch21/c/Test01_2.java
@@ -0,0 +1,51 @@
+// 애플리케이션 예외의 종류: Exception 계열의 예외와 RuntimeException 계열의 예외
+package ch21.c;
+
+import java.util.ArrayList;
+
+public class Test01_2 {
+
+  public static void main(String[] args) {
+
+    // 2) RuntimeException 계열의 예외
+    // => Exception 클래스의 서브 클래스이다.
+    // => 이 계열의 예외가 발생하는 경우에는 "예외 처리"가 필수가 아니다.
+    // 선택이다.
+    // 즉 try ~ catch를 쓰지 않아도 컴파일 오류가 발생하지 않는다.
+    // => 그러나 예외를 처리하지 않으면 메서드 호출자에게 예외가 전달된다.
+    // 메서드 호출자가 예외를 처리하지않으면 그 상위호출자에게 전달된다
+    // 그 상위 호출자가 예외를 처리하지 않으면 그 상위의 상위 호출자 에게 전달된다.
+    // 이런식으로 계속 전달 되다 보면 main() 메서드까지 전달 되고,
+    // main()에서도 예외를 처리하지 않으면 최종적으로 JVM에게 전달된다
+    // JVM이 예외를 받으면 그 즉시 프로그램을 멈춘다.
+    // 따라서 try ~ catch 사용을 강요받지 않더라도
+    // RuntumeException 예외를 처리하는것이 JVM을 멈추지 않게 하는 것이다.
+    //
+    // 예 :
+    //
+    int result = divide(100, 2); // ok!
+    System.out.println(result);
+    // RuntimeException 예외가 발생할 수 있는 메서드를 호출할 경우
+    // 컴파일러가 try ~catch~ 로 예외를 처리하라고 요구하지 않는다.
+    // 그렇다고 특정 예외 조건에서 예외가 발생되지 않는것은 아니다.
+    // 다만 이 메서드에서 처리하지 않으면
+    // 이 메서드를 호출한 쪽에 예외를 자동으로 전달한다.
+    // main()을 호출한 것은 JVM이기 떄문에
+    // main()에서 예외를 처리하지 않으면 JVM에게 전달되고
+    // JVM은 예외를 받는 즉시 실행을 멈춘다
+
+    result = divide(100, 0); // 예외발생 main()호출자에게 전달 . 즉 JVM에 전달. 즉시종료
+    System.out.println(result);
+    
+    System.out.println("실행 종료!");
+  }
+
+  static int divide(int a, int b) throws RuntimeException {
+    if (b == 0)
+      throw new RuntimeException("0으로 나눌 수 없습니다.");
+    return a / b;
+  }
+
+}
+
+
--- bitcamp-java-basic/src/main/java/ch21/c/Test03_2.java
+++ bitcamp-java-basic/src/main/java/ch21/c/Test03_2.java
@@ -5,9 +5,14 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Scanner;
 
-public class Test03_1 {
+public class Test03_2 {
 
-  public static void main(String[] args) throws Exception{ // main()의 호출자? JVM이다.
+  public static void main(String[] args) 
+      throws ClassNotFoundException, // 이 메서드에서 발생하는 예외의 목록을 지정한다.
+             NoSuchMethodException,  // 만약 메서드를 실행하는 중에 이런 예외가 발생한다면,
+             InstantiationException, // 즉시 이 메서드의 실행을 멈추고 
+             IllegalAccessException, // 이 메서드를 호출한 쪽에 예외 정보를 보고한다.
+             InvocationTargetException { // main()의 호출자? JVM이다.
     // Exception 계열의 예외 처리 방법
     // 1) try ~ catch로 예외 받기
     //    try {
@@ -27,14 +32,11 @@ public class Test03_1 {
     //    이런 예외를 한 곳에서 처리하고 싶을 때 이 방식을 사용한다.
     // => "어이~~ 여기에 예외 처리하지 마! 호출자에게 전달해." 라는 의미! 
     
-    int result = divide(100, 0);
-    System.out.println(result);
+    Scanner keyboard = new Scanner(System.in);
+    Class<?> clazz = Class.forName("ch21.c.PlusCommand");
+    Constructor<?> constructor = clazz.getConstructor(Scanner.class);
+    Command command = (Command) constructor.newInstance(keyboard);
+    command.execute();
   }
 
-  static int divide(int a, int b) throws Exception{
-    if (b== 0)
-      throw new Exception("0으로 나눌 수 없습니다.");
-    return a / b;
-  }
-  
 }
