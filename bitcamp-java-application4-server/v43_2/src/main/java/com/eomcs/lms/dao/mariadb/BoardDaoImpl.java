aee5baedde2a52e022c1ca1797694a0d784d9994
ohsangheon <ozzang1991@gmail.com>
2019-08-28 11:23:02 +0900

    bitcamp-java-application4 C/S 43_2

--- bitcamp-java-application4-server/v43_2/src/main/java/com/eomcs/lms/dao/mariadb/BoardDaoImpl.java
+++ bitcamp-java-application4-server/v43_2/src/main/java/com/eomcs/lms/dao/mariadb/BoardDaoImpl.java
@@ -9,68 +9,47 @@ import com.eomcs.lms.domain.Board;
 public class BoardDaoImpl implements BoardDao {
 
   SqlSessionFactory sqlSessionFactory;
-
+  
   public BoardDaoImpl(SqlSessionFactory sqlSessionFactory) {
     this.sqlSessionFactory = sqlSessionFactory;
   }
 
   @Override
   public int insert(Board board) throws Exception {
-    SqlSession sqlSession =sqlSessionFactory.openSession();
-    
-    try {
-        int count =sqlSession.insert("BoardDao.insert", board);
-        sqlSession.commit();
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      int count = sqlSession.insert("BoardDao.insert", board);
       return count;
-    } catch (Exception e) {
-      sqlSession.rollback();
-      throw e;
-      
-    } finally {
-      sqlSession.close();
     }
   }
 
   @Override
   public List<Board> findAll() throws Exception {
     try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-
       return sqlSession.selectList("BoardDao.findAll");
     }
   }
 
   @Override
   public Board findBy(int no) throws Exception {
-    SqlSession sqlSession = sqlSessionFactory.openSession();
-    try {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
       Board board = sqlSession.selectOne("BoardDao.findBy", no);
       if (board != null) {
-        sqlSession.update("BoardDao.increaseViewCount", no);
-        sqlSession.commit();
+          sqlSession.update("BoardDao.increaseViewCount", no);
       }
       return board;
-      
-    } catch (Exception e) {
-      sqlSession.rollback();
-      throw e;
-    } finally {
-      sqlSession.close();
     }
   }
 
   @Override
   public int update(Board board) throws Exception {
-    // openSession을 호출할 때 다음과 같이 autoCommit을 true로 설정 할 수 있다.
-    // 그러면 commit()을 따로 호출 하지 않아도 update()를 실행할 때 자동으로 commit 된다.
-    try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
       return sqlSession.update("BoardDao.update", board);
     }
   }
 
   @Override
   public int delete(int no) throws Exception {
-    
-    try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
       return sqlSession.delete("BoardDao.delete", no);
     }
   }
--- bitcamp-java-application4-server/v43_2/src/main/java/com/eomcs/lms/dao/mariadb/PhotoBoardDaoImpl.java
+++ bitcamp-java-application4-server/v43_2/src/main/java/com/eomcs/lms/dao/mariadb/PhotoBoardDaoImpl.java
@@ -1,42 +1,36 @@
 package com.eomcs.lms.dao.mariadb;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import com.eomcs.lms.dao.PhotoBoardDao;
 import com.eomcs.lms.domain.PhotoBoard;
-import com.eomcs.util.DataSource;
 
 public class PhotoBoardDaoImpl implements PhotoBoardDao {
+
   SqlSessionFactory sqlSessionFactory;
   
   public PhotoBoardDaoImpl(SqlSessionFactory sqlSessionFactory) {
-    this.sqlSessionFactory =sqlSessionFactory;
+    this.sqlSessionFactory = sqlSessionFactory;
   }
 
   @Override
   public int insert(PhotoBoard photoBoard) throws Exception {
-    try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
       return sqlSession.insert("PhotoBoardDao.insert", photoBoard);
     }
   }
 
   @Override
   public List<PhotoBoard> findAll() throws Exception {
-    try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
       return sqlSession.selectList("PhotoBoardDao.findAll");
     }
   }
 
   @Override
   public PhotoBoard findBy(int no) throws Exception {
-    try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
       PhotoBoard board = sqlSession.selectOne("PhotoBoardDao.findBy", no);
       if (board != null) {
         sqlSession.update("PhotoBoardDao.increaseViewCount", no);
@@ -47,64 +41,18 @@ public class PhotoBoardDaoImpl implements PhotoBoardDao {
 
   @Override
   public int update(PhotoBoard photoBoard) throws Exception {
-    try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
       return sqlSession.update("PhotoBoardDao.update", photoBoard);
     }
   }
 
   @Override
   public int delete(int no) throws Exception {
-    try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
       return sqlSession.delete("PhotoBoardDao.delete", no);
     }
   }
   
-  public static void main(String[] args) throws Exception {
-    try (Connection con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost/bitcampdb?user=bitcamp&password=1111");) {
-    
-      //PhotoBoardDao dao = new PhotoBoardDaoImpl(con);
-    
-      //1) insert() 테스트
-      /*
-      PhotoBoard b = new PhotoBoard();
-      b.setLessonNo(101);
-      b.setTitle("사진 게시글 테스트2");
-      
-      dao.insert(b);
-      */
-      
-      //2) findAll() 테스트
-      /*
-      List<PhotoBoard> list = dao.findAll();
-      for (PhotoBoard b : list) {
-        System.out.println(b);
-      }
-      */
-      
-      //3) findBy() 테스트
-      /*
-      PhotoBoard b = dao.findBy(9);
-      System.out.println(b);
-      */
-      
-      //4) update() 테스트
-      /*
-      PhotoBoard b = new PhotoBoard();
-      b.setNo(9);
-      b.setTitle("제목 변경");
-      dao.update(b);
-      */
-      
-      //5) delete() 테스트
-      /*
-      dao.delete(9);
-      */
-      
-      System.out.println("실행 완료!");
-    }
-  }
-
 }
 
 
