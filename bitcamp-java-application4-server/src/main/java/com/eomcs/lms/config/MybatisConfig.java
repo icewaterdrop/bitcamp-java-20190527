c5b7763055a73b2fc510a88e8e46f9da248fb21b
ohsangheon <ozzang1991@gmail.com>
2019-09-30 14:22:04 +0900

    bitcamp-java-application4 59_2

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/config/MybatisConfig.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/config/MybatisConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @MapperScan("com.eomcs.lms.dao")
 public class MybatisConfig {
   
